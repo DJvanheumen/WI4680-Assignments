763daaf6e0298a83810c54f9c9952e551b60992b
DJ <D.J.Heumen@student.tudelft.nl>
2026-04-17 22:51:23 +0200

    Updated useability

--- Assignment 2/code/RootFinding.py
+++ Assignment 2/code/RootFinding.py
@@ -33,78 +33,79 @@ def analyze_stability(eigenvalues):
     else:
         return "Stable/Unstable (need further study)"
 
+if __name__ == '__main__':
+    
+    # 1. Zero state
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    jacobian0 = calculate_jacobian(x1, x2, x3)
+    lambda0 = compute_eigenvalues(jacobian0)
+    results = analyze_stability(lambda0)
+    print(f'Zero state: ({x1}, {x2}, {x3})')
+    #print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobian0}')
+    #print(f'Eigenvalues: {lambda0}')
+    print(results)
 
-# 1. Zero state
-x1 = 0
-x2 = 0
-x3 = 0
-jacobian0 = calculate_jacobian(x1, x2, x3)
-lambda0 = compute_eigenvalues(jacobian0)
-results = analyze_stability(lambda0)
-print(f'Zero state: ({x1}, {x2}, {x3})')
-#print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobian0}')
-#print(f'Eigenvalues: {lambda0}')
-print(results)
+    # 2. Tumor cells only state
+    x1 = 1
+    x2 = 0
+    x3 = 0
+    jacobianT = calculate_jacobian(x1, x2, x3)
+    lambdaT = compute_eigenvalues(jacobianT)
+    results = analyze_stability(lambdaT)
+    print(f'Tumor state: ({x1}, {x2}, {x3})')
+    #print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobianT}')
+    #print(f'Eigenvalues: {lambdaT}')
+    print(results)
 
-# 2. Tumor cells only state
-x1 = 1
-x2 = 0
-x3 = 0
-jacobianT = calculate_jacobian(x1, x2, x3)
-lambdaT = compute_eigenvalues(jacobianT)
-results = analyze_stability(lambdaT)
-print(f'Tumor state: ({x1}, {x2}, {x3})')
-#print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobianT}')
-#print(f'Eigenvalues: {lambdaT}')
-print(results)
+    # 3. Healthy tissue cells state
+    x1 = 0
+    x2 = 1
+    x3 = 0
+    jacobianH = calculate_jacobian(x1, x2, x3)
+    lambdaH = compute_eigenvalues(jacobianH)
+    results = analyze_stability(lambdaH)
+    print(f'Healthy state: ({x1}, {x2}, {x3})')
+    #print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobianH}')
+    #print(f'Eigenvalues: {lambdaH}')
+    print(results)
 
-# 3. Healthy tissue cells state
-x1 = 0
-x2 = 1
-x3 = 0
-jacobianH = calculate_jacobian(x1, x2, x3)
-lambdaH = compute_eigenvalues(jacobianH)
-results = analyze_stability(lambdaH)
-print(f'Healthy state: ({x1}, {x2}, {x3})')
-#print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobianH}')
-#print(f'Eigenvalues: {lambdaH}')
-print(results)
+    # 4. Tumor and healthy tissue cells
+    x1 = (1 - P1)/(1 - P1*P4/P3)
+    x2 = 1 - P4/P3*x1
+    x3 = 0
+    jacobianTH = calculate_jacobian(x1, x2, x3)
+    lambdaTH = compute_eigenvalues(jacobianTH)
+    results = analyze_stability(lambdaTH)
+    print(f'Tumor and Healthy competition state: ({x1}, {x2}, {x3})')
+    #print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobianTH}')
+    #print(f'Eigenvalues: {lambdaTH}')
+    print(results)
 
-# 4. Tumor and healthy tissue cells
-x1 = (1 - P1)/(1 - P1*P4/P3)
-x2 = 1 - P4/P3*x1
-x3 = 0
-jacobianTH = calculate_jacobian(x1, x2, x3)
-lambdaTH = compute_eigenvalues(jacobianTH)
-results = analyze_stability(lambdaTH)
-print(f'Tumor and Healthy competition state: ({x1}, {x2}, {x3})')
-#print(f'Jacobian ({x1}, {x2}, {x3}) : \n {jacobianTH}')
-#print(f'Eigenvalues: {lambdaTH}')
-print(results)
+    # 5. Coexistence states
+    alpha = (P6*P7 + P8 - P5)
+    beta = P6*P7*P8
+    x1plus = -alpha/(2*P7) + np.sqrt(alpha**2 - 4*beta)/(2*P7)
+    x1min = -alpha/(2*P7)  - np.sqrt(alpha**2 - 4*beta)/(2*P7)
+    x1 = min(x1min, x1plus)
+    x2 = 1 - P4/P3*x1
+    x3 = (1 - x1 - P1*x2)/P2 
+    jacobianTHE = calculate_jacobian(x1, x2, x3)
+    lambdaTHE = compute_eigenvalues(jacobianTHE)
+    results = analyze_stability(lambdaTHE)
+    print(f'Coexistence state: ({x1}, {x2}, {x3})')
+    #print(f'Jacobian ({x1}, {x2}, {x3}): \n {jacobianTHE}')
+    #print(f'Eigenvalues: {lambdaTHE}')
+    print(results)
 
-# 5. Coexistence states
-alpha = (P6*P7 + P8 - P5)
-beta = P6*P7*P8
-x1plus = -alpha/(2*P7) + np.sqrt(alpha**2 - 4*beta)/(2*P7)
-x1min = -alpha/(2*P7)  - np.sqrt(alpha**2 - 4*beta)/(2*P7)
-x1 = min(x1min, x1plus)
-x2 = 1 - P4/P3*x1
-x3 = (1 - x1 - P1*x2)/P2 
-jacobianTHE = calculate_jacobian(x1, x2, x3)
-lambdaTHE = compute_eigenvalues(jacobianTHE)
-results = analyze_stability(lambdaTHE)
-print(f'Coexistence state: ({x1}, {x2}, {x3})')
-#print(f'Jacobian ({x1}, {x2}, {x3}): \n {jacobianTHE}')
-#print(f'Eigenvalues: {lambdaTHE}')
-print(results)
-
-x1 = max(x1min, x1plus)
-x2 = 1 - P4/P3*x1
-x3 = (1 - x1 - P1*x2)/P2
-jacobianTHE = calculate_jacobian(x1, x2, x3)
-lambdaTHE = compute_eigenvalues(jacobianTHE)
-results = analyze_stability(lambdaTHE)
-print(f'Coexistence state: ({x1}, {x2}, {x3})')
-#print(f'Jacobian ({x1}, {x2}, {x3}): \n {jacobianTHE}')
-#print(f'Eigenvalues: {lambdaTHE}')
-print(results)
+    x1 = max(x1min, x1plus)
+    x2 = 1 - P4/P3*x1
+    x3 = (1 - x1 - P1*x2)/P2
+    jacobianTHE = calculate_jacobian(x1, x2, x3)
+    lambdaTHE = compute_eigenvalues(jacobianTHE)
+    results = analyze_stability(lambdaTHE)
+    print(f'Coexistence state: ({x1}, {x2}, {x3})')
+    #print(f'Jacobian ({x1}, {x2}, {x3}): \n {jacobianTHE}')
+    #print(f'Eigenvalues: {lambdaTHE}')
+    print(results)
