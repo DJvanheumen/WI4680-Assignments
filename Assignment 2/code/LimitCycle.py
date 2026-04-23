eebdab5b86af62f6e7055fe8b3f42212b7ab60fe
DJ <D.J.Heumen@student.tudelft.nl>
2026-04-23 23:20:56 +0200

    Added LimitCycle.py

---
+++ Assignment 2/code/LimitCycle.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import RootFinding as rf
+import Continuation as con 
+
+
+# here we will find periodic solutions
+def find_periodic_solutions(period):
+    sol = []
+    return sol
+
+# stability of periodic solutions
+def stability_test(sol):
+    stable = []
+    return stable 
+
+if __name__ == "__main__":
+    period = con.period_limit_cycle
